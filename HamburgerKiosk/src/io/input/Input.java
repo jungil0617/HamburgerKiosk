b5fb97a36d04d0ca3f14fba430f06b28170d2b99
jungil0617 <jungil0617@naver.com>
2025-03-06 11:00:37 +0900

    Feat: Input 기능

---
+++ HamburgerKiosk/src/io/input/Input.java
@@ -0,0 +1,27 @@
+package io.input;
+
+import java.util.Scanner;
+
+public class Input {
+
+    private static Scanner SCANNER;
+
+    private Input() {
+    }
+
+    private static Scanner getInstance() {
+        if (SCANNER == null) {
+            SCANNER = new Scanner(System.in);
+        }
+        return SCANNER;
+    }
+
+    public static String nextLine() {
+        return getInstance().nextLine();
+    }
+
+    public static void close() {
+        SCANNER.close();
+    }
+
+}
