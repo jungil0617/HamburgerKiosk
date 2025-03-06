dec70c2aadd3cfbaf2af87840f32a2ce836bf389
jungil0617 <jungil0617@naver.com>
2025-03-06 11:22:41 +0900

    Chore: "Main" import 제거

--- HamburgerKiosk/src/Main.java
+++ HamburgerKiosk/src/Main.java
@@ -1,8 +1,4 @@
-import admin.Admin;
-import customer.Customer;
-import io.input.Input;
 import kiosk.Kiosk;
-import kiosk.Option;
 
 public class Main {
     public static void main(String[] args) {
