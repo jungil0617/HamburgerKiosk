7d44cf14d469f88fed184c96a163d3b4d8ba0df8
jungil0617 <jungil0617@naver.com>
2025-03-20 19:14:44 +0900

    Chore: InputMessage 변경

--- HamburgerKiosk/src/main/io/InputMessage.java
+++ HamburgerKiosk/src/main/io/InputMessage.java
@@ -11,8 +11,8 @@ public enum InputMessage {
     EXTRA_ORDER("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)"),
     CREATE_ADMIN("관리자 이름과 잔액을 입력해주세요."),
     LOGIN_ADMIN("로그인할 관리자 이름을 입력해주세요."),
-    CREATE_CUSTOMER(),
-    LOGIN_CUSTOMER();
+    CREATE_CUSTOMER("회원 번호화 잔액을 입력해주세요"),
+    LOGIN_CUSTOMER("로그인할 회원 번호를 입력해주세요.");
 
     private final String message;
 
